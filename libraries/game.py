17d8cd946f28c970d7a11a7c77d912da4bdf24af
arcana-coders <ryscarrillo@gmail.com>
2024-09-19 23:14:14 -0600

    game terminado

--- libraries/game.py
+++ libraries/game.py
@@ -1,31 +1,43 @@
 import random
 
-def main():
+def comput_think():
     while True:
         user_input = input("Please tell me a level: ")
         if user_input.isdigit() and int(user_input) != 0:
-            user_input_1 = int(user_input) + 1
+            user_input_1 = int(user_input)
             guess = (random.randint(1,user_input_1))
-            usr_guess = input("now guess what number am i thinking: ")
-            if usr_guess.isdigit() and int(usr_guess) != 0:
-                if int(usr_guess) < guess:
-                    print("Too small")
-                    continue
-                elif int(usr_guess) > guess:
-                    print("Too large!")
-                    continue
-                elif usr_guess == guess:
-                    print("Just right!")
-                    break
-
-            break
+            # print(guess)
+            return guess
+                
         else:
             continue
 
+def usr_guess():
+    guessing = comput_think()
+    while True:
+        
+        usr_guess = input("now guess what number am i thinking: ")
+        if usr_guess.isdigit() and int(usr_guess) != 0:
+                if int(usr_guess) < guessing:
+                    print("Too small")
+                    continue
+                elif int(usr_guess) > guessing:
+                    print("Too large!")
+                    continue
+                elif usr_guess == guessing:
+                    return(guessing)    
+        return(guessing)            
+                    
+        
+                    
 
 
-
-
+def main ():
+    right = usr_guess()
+    # number = comput_think()
+    print (f"just right, the number {right} is damn correct!")
+    
+    
 
 
 
