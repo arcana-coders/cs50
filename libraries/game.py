645d08a48f578c8835d361103f6845d4baf4fac2
arcana-coders <ryscarrillo@gmail.com>
2024-09-19 23:35:33 -0600

    game fixed

--- libraries/game.py
+++ libraries/game.py
@@ -3,42 +3,35 @@ import random
 def comput_think():
     while True:
         user_input = input("Please tell me a level: ")
-        if user_input.isdigit() and int(user_input) != 0:
+        if user_input.isdigit() and int(user_input) > 0:
             user_input_1 = int(user_input)
-            guess = (random.randint(1,user_input_1))
-            # print(guess)
+            guess = random.randint(1, user_input_1)
             return guess
-                
         else:
+            print("Invalid input. Please enter a positive number.")
             continue
 
 def usr_guess():
     guessing = comput_think()
     while True:
-        
-        usr_guess = input("now guess what number am i thinking: ")
-        if usr_guess.isdigit() and int(usr_guess) != 0:
-                if int(usr_guess) < guessing:
-                    print("Too small")
-                    continue
-                elif int(usr_guess) > guessing:
-                    print("Too large!")
-                    continue
-                elif usr_guess == guessing:
-                    return(guessing)    
-        return(guessing)            
-                    
-        
-                    
+        usr_guess = input("now guess what number am I thinking: ")
+        if usr_guess.isdigit():
+            usr_guess = int(usr_guess)
+            if usr_guess <= 0:
+                print("Invalid input. Please enter a positive number.")
+                continue
+            if usr_guess < guessing:
+                print("Too small!")
+            elif usr_guess > guessing:
+                print("Too large!")
+            else:
+                print("Just right!")
+                return guessing
+        else:
+            print("Invalid input. Please enter a positive number.")
 
+def main():
+    usr_guess()
 
-def main ():
-    right = usr_guess()
-    # number = comput_think()
-    print (f"just right, the number {right} is damn correct!")
-    
-    
-
-
-
-main ()
+if __name__ == "__main__":
+    main()
