f5e42ec021ddeb88d1cdf54bd8428eb9046d2b7e
arcana-coders <ryscarrillo@gmail.com>
2024-09-25 21:51:00 -0600

    Bitcoin terminado

--- libraries/bitcoin.py
+++ libraries/bitcoin.py
@@ -3,23 +3,32 @@ import json
 import sys
 
 def main ():
-    if len(sys.argv) < 2:
-        sys.exit("Too few arguments")
-    elif len(sys.argv) > 2:
-        sys.exit("Too much arguments")
-    elif len(sys.argv) == 2:
-        money_usr = sys.argv[1]
-        if money_usr.isalpha():
-            sys.exit("Thats not a number")
-        money_fl = float(money_usr)
-        if type(money_fl) == float:
-            print(f"Sir, yo have this {money_fl} bitcoins")
-        else:
-            print(f"you are giving this {money_usr}") 
+    try:
+        if len(sys.argv) < 2:
+            sys.exit("Too few arguments")
+        elif len(sys.argv) > 2:
+            sys.exit("Too much arguments")
+        elif len(sys.argv) == 2:
+            money_usr = sys.argv[1]
+            if money_usr.isalpha():
+                sys.exit("Thats not a number")
+            money_fl = float(money_usr)
+            if type(money_fl) == float:
+                btc_price = get_bitcoin()
+                money_btc = money_fl * btc_price
+                print(f"${money_btc:,.4f}")
+            else:
+                print(f"you are giving this {money_usr}") 
+    except requests.RequestException:
+        sys.exit("Not connection")
 
 def get_bitcoin():
     price_btc = requests.get ("https://api.coindesk.com/v1/bpi/currentprice.json")
-    print    
+    btc_js = price_btc.json()
+    
+    btc_qu = btc_js ["bpi"]["USD"]["rate_float"]
+    return btc_qu
+                    
 
 
 if __name__ == "__main__":
