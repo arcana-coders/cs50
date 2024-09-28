15025a481ab138d8d44b04a32a1244531937f208
arcana-coders <ryscarrillo@gmail.com>
2024-09-28 15:48:17 -0600

    test twitter code

--- unit_tests/twttr.py
+++ unit_tests/twttr.py
@@ -1,9 +1,22 @@
 def main ():
-    origin = input("what do you want to tweet? ")
-    print (shorten)
+    tweet = input("Gimme your tweet, Ill do the work for you: ")
+    print (shorten(tweet))
 
 def shorten(word):
-    return word.lower()
+    vocals = {
+        "a", "e", "i", "o", "u", 
+        "A", "E", "I", "O", "U"
+        }
+
+    ttstrips = word.strip()
+    #print (ttstrips)
+    ttclean = []
+    for letra in ttstrips:
+        if letra not in vocals:
+            ttclean.append(letra)
+    ttj = "".join(ttclean)
+    return ttj
+
 
 if __name__ == "__main__":
     main()
